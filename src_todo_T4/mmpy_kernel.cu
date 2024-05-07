a18bab3716c58b6c2090f3ee9a86d0256e904051
raj <suraj.s2306@gmail.com>
2024-05-07 11:35:36 -0700

    VERSION 1: Added Tiling and support for corner cases

--- src_todo_T4/mmpy_kernel.cu
+++ src_todo_T4/mmpy_kernel.cu
@@ -1,10 +1,10 @@
 // ;-*- mode: c;-*-
 // Matrix multiply device code
+#include "../src/types.h"
+#include "../src/utils.h"
+#include "mytypes.h"
 #include <assert.h>
 #include <math.h>
-#include "../src/utils.h"
-#include "../src/types.h"
-#include "mytypes.h"
 using namespace std;
 
 #include <stdio.h>
@@ -12,35 +12,48 @@ using namespace std;
 #ifdef NAIVE
 __global__ void matMul(int N, _FTYPE_ *C, _FTYPE_ *A, _FTYPE_ *B) {
 
-    int I =  blockIdx.y*blockDim.y + threadIdx.y;
-    int J =  blockIdx.x*blockDim.x + threadIdx.x;
+  int I = blockIdx.y * blockDim.y + threadIdx.y;
+  int J = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if((I < N) && (J < N)){
-        _FTYPE_ _c = 0;
-        for (unsigned int k = 0; k < N; k++) {
-            _FTYPE_ a = A[I * N + k];
-            _FTYPE_ b = B[k * N + J];
-            _c += a * b;
-        }
-        C[I * N + J] = _c;
+  if ((I < N) && (J < N)) {
+    _FTYPE_ _c = 0;
+    for (unsigned int k = 0; k < N; k++) {
+      _FTYPE_ a = A[I * N + k];
+      _FTYPE_ b = B[k * N + J];
+      _c += a * b;
     }
+    C[I * N + J] = _c;
+  }
 }
 
 #else
-//You should be changing the kernel here for the non naive implementation.
+// You should be changing the kernel here for the non naive implementation.
 __global__ void matMul(int N, _FTYPE_ *C, _FTYPE_ *A, _FTYPE_ *B) {
+  __shared__ double As[TILEDIM_M][TILEDIM_K], Bs[TILEDIM_K][TILEDIM_N];
 
-    int I =  blockIdx.y*blockDim.y + threadIdx.y;
-    int J =  blockIdx.x*blockDim.x + threadIdx.x;
+  int ty = threadIdx.y, tx = threadIdx.x;
+  int by = blockIdx.y, bx = blockIdx.x;
 
-    if((I < N) && (J < N)){
-        _FTYPE_ _c = 0;
-        for (unsigned int k = 0; k < N; k++) {
-            _FTYPE_ a = A[I * N + k];
-            _FTYPE_ b = B[k * N + J];
-            _c += a * b;
-        }
-        C[I * N + J] = _c;
-    }
+  int I = by * TILEDIM_K + ty;
+  int J = bx * TILEDIM_K + tx;
+
+  double Cij = 0;
+  for (int kk = 0; kk < (N / TILEDIM_M + (N % TILEDIM_M != 0)); kk++) {
+    if (I < N && kk * TILEDIM_M + tx < N)
+      As[ty][tx] = A[I * N + kk * TILEDIM_M + tx];
+    else
+      As[ty][tx] = 0;
+    if (kk * TILEDIM_M + ty < N && J < N)
+      Bs[ty][tx] = B[(kk * TILEDIM_M + ty) * N + J];
+    else
+      Bs[ty][tx] = 0;
+
+    __syncthreads();
+    for (int k = 0; k < TILEDIM_K; k++)
+      Cij += As[ty][k] * Bs[k][tx];
+    __syncthreads();
+    if (I < N && J < N)
+      C[I * N + J] = Cij;
+  }
 }
 #endif
