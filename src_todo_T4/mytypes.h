fe4486636edebb887e4908a22604b642651e2419
raj <suraj.s2306@gmail.com>
2024-05-07 11:34:28 -0700

    working with 32*32 block size

--- src_todo_T4/mytypes.h
+++ src_todo_T4/mytypes.h
@@ -12,18 +12,18 @@
 #define TILESCALE_K 1 // Enter your own values
 #endif
 
-#define TILEDIM_M 1 // Enter your own values
-#define TILEDIM_N 1 // Enter your own values
+#define TILEDIM_M 32 // Enter your own values
+#define TILEDIM_N 32 // Enter your own values
 
 // matrix A loads
 // with warps along the horiziontal axis (K)
 // so to get good coalescaed loads, we want TILEDIM_K to be >= 32
 //
-#define TILEDIM_K 1 // Enter your own values
+#define TILEDIM_K 32 // Enter your own values
 
 // step size in each dimension
 #define TILESTEP_N 1 // Enter your own values
 #define TILESTEP_K 1 // Enter your own values
 #define TILESTEP_M 1 // Enter your own values
- 
+//
 #endif
