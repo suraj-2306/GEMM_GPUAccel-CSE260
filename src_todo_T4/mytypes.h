c3489fb438d84c593d65da8b6647d4518a8d3d6d
raj <suraj.s2306@gmail.com>
2024-05-13 22:47:47 -0700

    changed the block size to 64

--- src_todo_T4/mytypes.h
+++ src_todo_T4/mytypes.h
@@ -3,23 +3,23 @@
 
 // tilescale (# of points computed by each thread)
 #ifndef TILESCALE_M
-#define TILESCALE_M 2 // Enter your own values
+#define TILESCALE_M 4 // Enter your own values
 #endif
 #ifndef TILESCALE_N
-#define TILESCALE_N 2 // Enter your own values
+#define TILESCALE_N 4 // Enter your own values
 #endif
 #ifndef TILESCALE_K
 #define TILESCALE_K 1 // Enter your own values
 #endif
 
-#define TILEDIM_M 32 // Enter your own values
-#define TILEDIM_N 32 // Enter your own values
+#define TILEDIM_M 64 // Enter your own values
+#define TILEDIM_N 64 // Enter your own values
 
 // matrix A loads
 // with warps along the horiziontal axis (K)
 // so to get good coalescaed loads, we want TILEDIM_K to be >= 32
 //
-#define TILEDIM_K 32 // Enter your own values
+#define TILEDIM_K 64 // Enter your own values
 
 // step size in each dimension
 #define TILESTEP_N 1 // Enter your own values
